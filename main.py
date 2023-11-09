84a21d987c71824091493ffb422351c0dc0e9b70
Henry Cuzco <40706933+uiucmeche1317@users.noreply.github.com>
2023-11-08 20:24:31 -0600

    Updated weather query variable
    The query was updated to include a year range and a state input parameter.

--- main.py
+++ main.py
@@ -7,23 +7,25 @@ import matplotlib.pyplot as plt
 import os
 #%%
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(Path.cwd() / "secrets" / "my_credentials.json")
-def get_weather_dataset(year, state):
-    """This function runs a query on Google BigQuery from the noaa_gsod gsod dataset and returns a dataframe based on the year. The data is filtered by year and by state.
+def get_weather_dataset(start_year, end_year, state):
+    """This function runs a query on Google BigQuery from the noaa_gsod gsod dataset and returns a dataframe between a start_year and an end_year. The data is filtered by state.
 
     Args:
         year (int): the year in which we want to obtain the weather data
 
     Returns:
-        pandas.DataFrame: the data that pertains to a particular year and state
+        pandas.DataFrame: the data that pertains to a year range and state
     """
-    weather_query = f""" 
-    SELECT * EXCEPT (wban, stn, usaf, country, state, call) FROM `bigquery-public-data.noaa_gsod.gsod{year}` AS gsod{year} 
-    INNER JOIN `bigquery-public-data.noaa_gsod.stations` AS stations ON gsod{year}.wban = stations.wban WHERE state = :state """
+    weather_query = f"""
+    SELECT gsod_combined.* EXCEPT (wban, stn, usaf, country, state, call) 
+    FROM `bigquery-public-data.noaa_gsod.gsod*` as gsod_combined
+    JOIN `bigquery-public-data.noaa_gsod.stations` AS stations ON gsod_combined.wban = stations.wban 
+    WHERE stations.state = '{state}' AND gsod_combined.year BETWEEN {start_year} AND {end_year};
+    """
     weather_df = pd.read_gbq(weather_query, progress_bar_type = "tqdm")
     return weather_df
 #%%
 # Gather the datasets for the weather of each year from 1990 to 2023
-weather_data = {year: get_weather_dataset(year, "IL") for year in range(1990, 2024)}
-combined_weather_data = pd.concat([wdf for wdf in weather_data])
+combined_weather_data = get_weather_dataset(1990, 2023, "IL")
 combined_weather_data.replace({999.9: np.nan}, inplace = True)
 combined_weather_data.replace({9999.9: np.nan}, inplace = True)
