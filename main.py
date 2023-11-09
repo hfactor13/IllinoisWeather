125cd3b0903ecb63213d5968f9a2028dfe2e6431
Henry Cuzco <40706933+uiucmeche1317@users.noreply.github.com>
2023-11-08 19:45:08 -0600

    Combining the weather datasets

--- main.py
+++ main.py
@@ -16,12 +16,14 @@ def get_weather_dataset(year, state):
     Returns:
         pandas.DataFrame: the data that pertains to a particular year and state
     """
-    weather_query = f"""
-SELECT * EXCEPT (wban, stn, usaf, country, state, call) FROM `bigquery-public-data.noaa_gsod.gsod{year}` AS gsod{year} 
-INNER JOIN `bigquery-public-data.noaa_gsod.stations` AS stations ON gsod{year}.wban = stations.wban WHERE state = {state}
-"""
+    weather_query = f""" 
+    SELECT * EXCEPT (wban, stn, usaf, country, state, call) FROM `bigquery-public-data.noaa_gsod.gsod{year}` AS gsod{year} 
+    INNER JOIN `bigquery-public-data.noaa_gsod.stations` AS stations ON gsod{year}.wban = stations.wban WHERE state = :state """
     weather_df = pd.read_gbq(weather_query, progress_bar_type = "tqdm")
     return weather_df
 #%%
 # Gather the datasets for the weather of each year from 1990 to 2023
 weather_data = {year: get_weather_dataset(year, "IL") for year in range(1990, 2024)}
+combined_weather_data = pd.concat([wdf for wdf in weather_data])
+combined_weather_data.replace({999.9: np.nan}, inplace = True)
+combined_weather_data.replace({9999.9: np.nan}, inplace = True)
